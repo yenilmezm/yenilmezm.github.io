b2b9b879d8ee4741d7ae091a6881ebdd5341a71e
yenilmezm <mert.yenilmez@tedu.edu.tr>
2022-10-13 10:14:20 +0200

    get weather api call

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -7,5 +7,10 @@ import { HttpClient } from "@angular/common/http";
 export class ApixuService {
   constructor(private http: HttpClient) { }
 
-
+  getWeather(location) {
+    return this.http.get(
+      `http://api.weatherstack.com/current?access_key=33f96cd433b577d49cfd92cd5423626c&query=` +
+      location
+    );
+  }
 }
